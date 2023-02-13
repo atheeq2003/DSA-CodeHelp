1430c1db9917d9a9b648289c5982d0f3063e7702
atheeq_2003 <atheequrrahaman8@gmail.com>
2023-02-13 21:04:41 +0530

    Pattern 17

---
+++ L4_12_Pattern12.cpp
@@ -0,0 +1,21 @@
+//Pattern 17
+#include<iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cin >> n;
+    int i = 1;
+    while(i <= n)
+    {
+        int j = 1;
+        while(j <= i)
+        {
+            char ch = 'A'+i+j-2;
+            cout << ch << " ";
+            j++; 
+        }
+        cout << endl;
+        i++;
+    }
+}
